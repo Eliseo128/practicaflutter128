217f0dfa03512c6de726053f06ce78d8d66ac02e
Manuel Nava <navaweb@cbtis128.edu.mx>
2025-02-15 16:58:28 +0000

    mario

---
+++ nopal_app/lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:nopal_app/pagina02.dart';
+
+
+void main() => runApp(MyApp()); // llama al primer widget que se ejecutará
+
+/*
+void main() {
+    runApp(MyApp);
+}*/
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // es el estilo de la app
+      title: "Navegacion", // nombre de la app
+      home: Inicio(),
+    );
+  }
+}
+
+class Inicio extends StatefulWidget {
+  const Inicio({super.key});
+
+  @override
+  State<Inicio> createState() => _InicioState();
+}
+
+class _InicioState extends State<Inicio> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // estructura básica de una app
+      // estructura de una app movil
+      appBar: AppBar(
+        title: Text("Navegacion"),
+      ),
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Container(
+          color: Colors.blue, // Color de fondo del contenedor
+
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Principal"),
+              ElevatedButton(
+                  onPressed: () => {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) => Pagina2()))
+                      },
+                  child: Text("Ir otra página"))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
